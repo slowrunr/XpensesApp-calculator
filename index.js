df5ad318cc5f5791dc7d5ebce740fdd1890bd5ae
slowrunr <mungaslov.nft@gmail.com>
2023-07-30 22:23:17 +0900

    added expensesList output

--- index.js
+++ index.js
@@ -4,9 +4,26 @@ const addSumBtnNode = document.getElementById("addSumBtn");
 const expensesNode = document.getElementById("expenses");
 
 addSumBtnNode.addEventListener("click", function () {
-  const expense = expensesInputNode.value;
+  //1. receive data from input
+  if (!expensesInputNode.value === "") {
+    return;
+  }
+
+  const expense = parseInt(expensesInputNode.value);
+  expensesInputNode.value = "";
+
+  //2. save data
   expenses.push(expense);
   console.log(expenses);
-  //addExpense(expensesFromUser);
-  //renderExpenses();
+
+  //3. render list of expenses
+  let expensesListHTML = "";
+  expenses.forEach((element) => {
+    // цикл forEach нужен, чтобы поработать с каждым элементом списка и что-то в нём изменить
+    // - полная запись работы с элементом, но можно сократить.
+    // const elementHTML = `<li>${element}</li>;
+    //expensesListHtml += elementHTML;
+    expensesListHTML += `<li>${element}</li>`; // сокращенная запись работы с циклом
+  });
+  expensesNode.innerHTML = `<ol>${expensesListHTML}</ol>`;
 });
