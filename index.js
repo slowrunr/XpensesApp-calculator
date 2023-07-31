60b0ba4034362ec040871918f1e22804b9134cf5
slowrunr <mungaslov.nft@gmail.com>
2023-07-31 23:15:05 +0900

    added sum function for all expenses

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const expenses = [];
 const expensesInputNode = document.getElementById("expensesInput");
 const addSumBtnNode = document.getElementById("addSumBtn");
 const expensesNode = document.getElementById("expenses");
+const sumUpNode = document.getElementById("sumUp");
 
 addSumBtnNode.addEventListener("click", function () {
   //1. receive data from input
@@ -19,11 +20,20 @@ addSumBtnNode.addEventListener("click", function () {
   //3. render list of expenses
   let expensesListHTML = "";
   expenses.forEach((element) => {
-    // цикл forEach нужен, чтобы поработать с каждым элементом списка и что-то в нём изменить
-    // - полная запись работы с элементом, но можно сократить.
-    // const elementHTML = `<li>${element}</li>;
-    //expensesListHtml += elementHTML;
-    expensesListHTML += `<li>${element}</li>`; // сокращенная запись работы с циклом
+    expensesListHTML += `<li>${element}</li> руб.`; // сокращенная запись работы с циклом
   });
+  // цикл forEach нужен, чтобы поработать с каждым элементом списка и что-то в нём изменить
+  // - полная запись работы с элементом, но можно сократить.
+  // const elementHTML = `<li>${element}</li>;
+  //expensesListHtml += elementHTML;
   expensesNode.innerHTML = `<ol>${expensesListHTML}</ol>`;
+
+  //4. sum up and render total
+  let sum = 0;
+  expenses.forEach((element) => {
+    sum += element;
+  });
+  //console.log(sum);
+
+  sumUpNode.innerText = sum;
 });
